c120222a27e98d3b20d3eb5d1ca07addfaefc40a
minseo jang <mingdo.dev@gmail.com>
2023-09-13 21:25:33 +0900

    Part1: 03 컴포넌트와 prop

--- first-my-app/App.js
+++ first-my-app/App.js
@@ -1,36 +1,76 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, TextInput, ScrollView, Button, Switch } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native'
+import React from 'react'
+
+const Header = (props) => {
+  return <Text>{props.tilte}</Text>
+};
+const MyProfile = () => {
+  return <Profile
+        name="민서"
+        uri="https://i.pinimg.com/236x/ea/33/d8/ea33d8bfd81b4f86beebdba49011e157.jpg"
+        profileSize={40}
+        />
+};
+const Division = () => {
+  return <Text>Division</Text>
+};
+const FriendSection = () => {
+  return <Text>FriendSection</Text>
+};
+const FriendList = () => {
+  return (
+    <View>
+      <Profile name="고양이" uri="https://i.pinimg.com/236x/ea/33/d8/ea33d8bfd81b4f86beebdba49011e157.jpg" profileSize={30} />
+      <Profile name="쿼카" uri="https://i.pinimg.com/236x/ea/33/d8/ea33d8bfd81b4f86beebdba49011e157.jpg" profileSize={30} />
+      <Profile name="강아지" uri="https://i.pinimg.com/236x/ea/33/d8/ea33d8bfd81b4f86beebdba49011e157.jpg" profileSize={30} />
+      <Profile name="코뿔소" uri="https://i.pinimg.com/236x/ea/33/d8/ea33d8bfd81b4f86beebdba49011e157.jpg" profileSize={30} />
+      <Profile name="거북이" uri="https://i.pinimg.com/236x/ea/33/d8/ea33d8bfd81b4f86beebdba49011e157.jpg" profileSize={30} />
+    </View>
+  )
+};
+
+// 함수형 컴포넌트
+const Profile = (props) => {
+  return (
+    <View style={{ flexDirection: 'row' }}>
+      <Image
+        source={{ uri: props.uri }}
+        style={{
+          height: props.profileSize,
+          width: props.profileSize,
+          borderRadius: 50 }}
+      />
+      <Text>{props.name}</Text>
+    </View>
+  )
+}
+
+// 클래스형 컴포넌트
+// class Profile extends React.Component {
+//   render() {
+//     return (
+//       <View style={{ flexDirection: 'row' }}>
+//        <Image
+//         source={{ uri: this.props.uri }}
+//         style={{
+//           height: this.props.profileSize,
+//           width: this.props.profileSize,
+//           borderRadius: 50 }}
+//         />
+//       <Text>{this.props.name}</Text>
+//     </View>
+//     )
+//   }
+// }
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hey hey hey !!!!! </Text>
-      
-      <Image source={require('./moon.png')} style={styles.localImg}/>
-      <Image source={{uri:'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Felis_catus-cat_on_snow.jpg/269px-Felis_catus-cat_on_snow.jpg'}}
-      style={styles.urlImg} />
-
-    <TextInput placeholder='이름' />
-
-    <Button title="Click Me!" onPress={() => {
-      console.log('hi');
-    }}></Button>
-
-    <Switch value={false}></Switch>
-
-    <ScrollView>
-      <Image source={{uri:'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Felis_catus-cat_on_snow.jpg/269px-Felis_catus-cat_on_snow.jpg'}} style={styles.urlImg} />
-      <Image source={{uri:'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Felis_catus-cat_on_snow.jpg/269px-Felis_catus-cat_on_snow.jpg'}} style={styles.urlImg} />
-      <Image source={{uri:'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Felis_catus-cat_on_snow.jpg/269px-Felis_catus-cat_on_snow.jpg'}} style={styles.urlImg} />
-      <Image source={{uri:'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Felis_catus-cat_on_snow.jpg/269px-Felis_catus-cat_on_snow.jpg'}} style={styles.urlImg} />
-      <Image source={{uri:'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Felis_catus-cat_on_snow.jpg/269px-Felis_catus-cat_on_snow.jpg'}} style={styles.urlImg} />
-      <Image source={{uri:'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Felis_catus-cat_on_snow.jpg/269px-Felis_catus-cat_on_snow.jpg'}} style={styles.urlImg} />
-      <Image source={{uri:'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Felis_catus-cat_on_snow.jpg/269px-Felis_catus-cat_on_snow.jpg'}} style={styles.urlImg} />
-      <Image source={{uri:'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Felis_catus-cat_on_snow.jpg/269px-Felis_catus-cat_on_snow.jpg'}} style={styles.urlImg} />
-    </ScrollView>
-
-    </View>
-  );
+  return <View style={styles.container}>
+    <Header title="친구" />
+    <MyProfile />
+    <Division />
+    <FriendSection />
+    <FriendList />
+  </View>
 }
 
 const styles = StyleSheet.create({
@@ -40,16 +80,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  text: {
-    fontWeight: 'bold',
-    fontSize: 20,
-  },
-  localImg: {
-    width: 100,
-    height:100
-  },
-  urlImg: {
-    width: 100,
-    height: 100
-  }
-});
+})
