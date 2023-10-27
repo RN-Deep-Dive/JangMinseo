50a0c2f0a958dba6a7a6688a3f71005542c70230
minseo jang <mingdo.dev@gmail.com>
2023-10-27 23:55:46 +0900

    Part2: 계산기 예외 처리

--- calculator/src/Calculator.js
+++ calculator/src/Calculator.js
@@ -54,12 +54,20 @@ export default () => {
   const [result, setResult] = useState(null); // 그때 그때의 result
   const [tempInput, setTempInput] = useState(null); // 저장되어있는 input 값
   const [tempOperator, setTempOperator] = useState(null); // 저장되어있는 연산자 값
-  
+  const [isClickedOperator, setIsClickedOperator] = useState(false);
+  const [isClickedEqual, setIsClickedEqual] = useState(false);
+
+//   const hasInput = input ? true : false;
+  const hasInput = !!input;
+
 const onPressNum = (num) => {
-    if (currentOperator) {
+    // 연산을 하는 중이고, 방금 연산자 버튼을 눌렀음 -> input에 새로 값 저장
+    if (currentOperator && isClickedOperator) {
         setResult(input); // 원래 input 저장
         setInput(num); // 아니근데이러며는 한자리수연산밖에 못해???????????
+        setIsClickedOperator(false);
     } else {
+        // 연산을 하는 중이 아니거나, 다음 피연산자가 두 자리 수 이상일 때
         const newInput = Number(`${input}${num}`);
     setInput(newInput);
     }
@@ -68,35 +76,48 @@ const onPressNum = (num) => {
 const onPressOperator = (operator) => {
     if (operator !== "=") {
         setCurrentOperator(operator);
+        setIsClickedOperator(true);
+        setIsClickedEqual(false);
     } else {
         let finalResult = result;
-        switch(currentOperator) {
+        const finalInput = isClickedEqual? tempInput : input;
+        const finalOperator = isClickedEqual? tempOperator : currentOperator;
+        switch(finalOperator) {
             case '+':
-                finalResult = result + input;
+                finalResult = result + finalInput;
                 break;
             case '-':
-                finalResult = result - input;
+                finalResult = result - finalInput;
                 break;
             case '*':
-                finalResult = result * input;
+                finalResult = result * finalInput;
                 break;
             case '/':
-                finalResult = result / input;
+                finalResult = result / finalInput;
                 break;
             default:
                 break;
         }
         setResult(finalResult);
         setInput(finalResult);
+        setTempInput(finalInput);
+        setCurrentOperator(null);
+        setTempOperator(finalOperator);
+        setIsClickedEqual(true);
     }
 }
 
 const onPressReset = () => {
-    setInput(0);
-    setCurrentOperator(null);
-    setResult(null);
-    setTempInput(null);
-    setTempOperator(null);
+    if (hasInput) {
+        setInput(0);
+    } else {
+        setInput(0);
+        setCurrentOperator(null);
+        setResult(null);
+        setTempInput(null);
+        setTempOperator(null);
+    }
+    
 }
 
   return (
@@ -117,7 +138,7 @@ const onPressReset = () => {
       <ButtonContainer>
         <Button
             type="reset"
-            text="AC"
+            text={hasInput ? "C" : "AC"}
             onPress={onPressReset}
             flex={3}
         />
