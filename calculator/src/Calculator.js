30e28b164b06a1d0510211a60703bba90358afc1
minseo jang <mingdo.dev@gmail.com>
2023-10-28 00:03:27 +0900

    Part2: 계산기 custom hook - useCalculator

--- calculator/src/Calculator.js
+++ calculator/src/Calculator.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { View, TouchableOpacity, Text } from 'react-native';
 import styled from 'styled-components/native';
+import { useCalculator } from './use-calculator';
 
 const COLOR = {
     RESULT: '#4e4c51',
@@ -49,76 +50,18 @@ const InputContainer = styled.View`
 `; // 숫자들이 늘어나면 그 크기에 따라 늘어나도록 할 것임
 
 export default () => {
-  const [input, setInput] = useState(0); // input
-  const [currentOperator, setCurrentOperator] = useState(null); // 현재 연산자
-  const [result, setResult] = useState(null); // 그때 그때의 result
-  const [tempInput, setTempInput] = useState(null); // 저장되어있는 input 값
-  const [tempOperator, setTempOperator] = useState(null); // 저장되어있는 연산자 값
-  const [isClickedOperator, setIsClickedOperator] = useState(false);
-  const [isClickedEqual, setIsClickedEqual] = useState(false);
-
-//   const hasInput = input ? true : false;
-  const hasInput = !!input;
-
-const onPressNum = (num) => {
-    // 연산을 하는 중이고, 방금 연산자 버튼을 눌렀음 -> input에 새로 값 저장
-    if (currentOperator && isClickedOperator) {
-        setResult(input); // 원래 input 저장
-        setInput(num); // 아니근데이러며는 한자리수연산밖에 못해???????????
-        setIsClickedOperator(false);
-    } else {
-        // 연산을 하는 중이 아니거나, 다음 피연산자가 두 자리 수 이상일 때
-        const newInput = Number(`${input}${num}`);
-    setInput(newInput);
-    }
-}
-
-const onPressOperator = (operator) => {
-    if (operator !== "=") {
-        setCurrentOperator(operator);
-        setIsClickedOperator(true);
-        setIsClickedEqual(false);
-    } else {
-        let finalResult = result;
-        const finalInput = isClickedEqual? tempInput : input;
-        const finalOperator = isClickedEqual? tempOperator : currentOperator;
-        switch(finalOperator) {
-            case '+':
-                finalResult = result + finalInput;
-                break;
-            case '-':
-                finalResult = result - finalInput;
-                break;
-            case '*':
-                finalResult = result * finalInput;
-                break;
-            case '/':
-                finalResult = result / finalInput;
-                break;
-            default:
-                break;
-        }
-        setResult(finalResult);
-        setInput(finalResult);
-        setTempInput(finalInput);
-        setCurrentOperator(null);
-        setTempOperator(finalOperator);
-        setIsClickedEqual(true);
-    }
-}
-
-const onPressReset = () => {
-    if (hasInput) {
-        setInput(0);
-    } else {
-        setInput(0);
-        setCurrentOperator(null);
-        setResult(null);
-        setTempInput(null);
-        setTempOperator(null);
-    }
-    
-}
+    // Hook 분리: UI에 집중, custom hook에서는 로직에 집중할 수 있게 해줌 / 유지 보수에 용이
+    const {
+        input,
+        currentOperator,
+        result,
+        tempInput,
+        tempOperator,
+        hasInput,
+        onPressNum,
+        onPressOperator,
+        onPressReset,
+    } = useCalculator(); // 구조분해
 
   return (
     <View style={{ flex: 1, width: 250, justifyContent: 'center' }}>
