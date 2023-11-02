799610bbcdc5e500097413b66e71c47c725623f9
minseo jang <mingdo.dev@gmail.com>
2023-11-02 16:53:39 +0900

    Part3: 로또 Animation

--- lotto-number-get/src/components/LottoNumberView.js
+++ lotto-number-get/src/components/LottoNumberView.js
@@ -1,8 +1,10 @@
-import { View } from 'react-native'
-import React, {useCallback} from 'react'
+import { View, Animated } from 'react-native'
+import React, {useCallback, useEffect, useState} from 'react'
 import { Typography } from './Typography'
 
 export const LottoNumberView = (props) => {
+    const [viewHeight, setViewHeight] = useState(0);
+    const [animatedValue] = useState(new Animated.Value(1));
 
     const getNumberBackgroundColor = useCallback(()=>{
         const randomNumber = Math.floor((Math.random() * 10)) % 6
@@ -25,15 +27,54 @@ export const LottoNumberView = (props) => {
         return 'black';
     })
 
+    const translateY = animatedValue.interpolate({
+        inputRange:[0, 1],
+        outputRange: [-viewHeight * 0.6, 0]
+    })
+
+    useEffect(()=>{
+        animatedValue.setValue(0);
+
+        Animated.timing(animatedValue, {
+            duration:1000,
+            toValue:1,
+        }).start();
+    },[props.numbers])
+
   return (
-    <View style={{flex:1, flexDirection:'row', alignItems:'center', justifyContent:'space-between'}}>
+    <View
+     style={{
+        overflow:'hidden',
+        flex:1,
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'space-between'
+    }}
+     onLayout={({nativeEvent})=>{
+        console.log(nativeEvent.layout);
+        setViewHeight(nativeEvent.layout.height);
+     }}>
         {props.numbers.map((item)=>{
             return (
-                <View style={{backgroundColor:getNumberBackgroundColor(), width:40, height:40, borderRadius:20, alignItems:'center', justifyContent:'center'}}>
+                <Animated.View
+                 style={{
+                    backgroundColor:getNumberBackgroundColor(), 
+                    width:40,
+                    height:40,
+                    borderRadius:20,
+                    alignItems:'center', 
+                    justifyContent:'center',
+                    transform:[
+                        {
+                            translateY: translateY
+                        }
+                    ]
+                     // overflowHidden은 상위 컴포넌트에 준다. 
+                      }}>
                     <Typography fontSize={20} color='white'>
                         {item}
                     </Typography>
-                </View>
+                </Animated.View>
             )
         })}
     </View>
